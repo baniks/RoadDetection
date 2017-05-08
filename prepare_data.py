a2704b33faaa2030633379bcd9b165275836a3a8
soubarna <soubarna.banik@gmail.com>
2017-05-08 15:22:51 +0200

    addes sub dataset

--- prepare_data.py
+++ prepare_data.py
@@ -12,8 +12,7 @@ hymap02 = open_image("data/Hyperspectral data/Berlin Urban Gradient 2009 01 imag
 # hymap02_ds01 = hymap02[150:500, 90:700, :]
 hymap02_ds01 = hymap02[1500:2000, 50:630, :]
 hymap02_ds02 = hymap02[2000:2400, 40:630, :]
-hymap02_ds02_sub_img = hymap02[2000:2400, 40:630, :]
-
+hymap02_ds02_sub_img = hymap02[2030:2130, 70:170, :]
 # display
 view1 = imshow(hymap02_ds01, bands=(27, 74, 13,))
 view2 = imshow(hymap02_ds02, bands=(27, 74, 13,))
@@ -21,6 +20,7 @@ view2 = imshow(hymap02_ds02, bands=(27, 74, 13,))
 # saving dataset
 cPickle.dump(hymap02_ds01, open("data/hymap02_ds01.pkl", "wb"))
 cPickle.dump(hymap02_ds02, open("data/hymap02_ds02.pkl", "wb"))
+cPickle.dump(hymap02_ds02_sub_img, open("data/hymap02_ds02_sub_img.pkl", "wb"))
 save_rgb('images/hymap02/ds01/hymap02_ds01.jpg', hymap02_ds01, [18, 5, 1])
 save_rgb('images/hymap02/ds01/hymap02_ds01_27_74_13.jpg', hymap02_ds01, [27, 74, 13])
 save_rgb('images/hymap02/ds01/hymap02_ds01_grey.jpg', hymap02_ds01, [5,])
@@ -30,6 +30,9 @@ save_rgb('images/hymap02/ds02/hymap02_ds02.jpg', hymap02_ds02, [18, 5, 1])
 save_rgb('images/hymap02/ds02/hymap02_ds02_27_74_13.jpg', hymap02_ds02, [27, 74, 13])
 save_rgb('images/hymap02/ds02/hymap02_ds02_grey.jpg', hymap02_ds02, [5,])
 save_rgb('images/hymap02/ds02/hymap02_ds02_infra.jpg', hymap02_ds02, [39,])
+save_rgb('images/hymap02/ds02/hymap02_ds02_sub_img_27_74_13.jpg', hymap02_ds02_sub_img, [27, 74, 13])
+save_rgb('images/hymap02/ds02/hymap02_ds02_sub_img_infra.jpg', hymap02_ds02_sub_img, [39,])
+
 
 # Scene: hymap01
 hymap01 = open_image("data/Hyperspectral data/Berlin Urban Gradient 2009 01 image products/01_image_products/HyMap01_Berlin_Urban_Gradient_2009.hdr")
